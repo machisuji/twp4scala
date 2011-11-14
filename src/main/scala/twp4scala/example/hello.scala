07edf9738201889100e7ee658a781c194d4d2f1b
Markus Kahl <machisuji@googlemail.com>
2011-11-14 10:21:31 +0100

    take args

--- src/main/scala/twp4scala/example/hello.scala
+++ src/main/scala/twp4scala/example/hello.scala
@@ -5,12 +5,13 @@ import twp4scala._
 
 object Hello extends App {
   val echo = new Echo with TcpConnection {
-    def host = "www.dcl.hpi.uni-potsdam.de"
-    def port = 80
+    def host = args.drop(1).headOption              getOrElse "www.dcl.hpi.uni-potsdam.de"
+    def port = args.drop(2).headOption.map(_.toInt) getOrElse 80
   }
+  val msg = args.headOption getOrElse "Hallo Welt!"
 
   Client.run(echo) { echo =>
-    val (text, letters) = echo.echo("Hallo Welt!")
+    val (text, letters) = echo.echo(msg)
     println("\"%s\" contains %d letters.".format(text, letters))
   }
 }
