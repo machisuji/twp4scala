3e4847418d3310c3438888b0f0cfcff768d8f978
Markus Kahl <machisuji@googlemail.com>
2011-11-27 19:07:52 +0100

    + generic TcpServer

--- src/main/scala/twp4scala/example/server.scala
+++ src/main/scala/twp4scala/example/server.scala
@@ -5,54 +5,16 @@ import java.net._
 import twp4scala._
 import twp4scala.protocol.echo._
 
-class EchoServer(val port: Int) extends Runnable {
+class EchoServer(val port: Int) extends TcpServer {
 
-  @volatile var cancel = false
-
-  def run() {
-    val socket = new ServerSocket(port)
-    println("EchoServer listening on port " + port + " ...")
-    try {
-      while (!cancel) {
-        val client = socket.accept
-        val whois = client.getInetAddress.getHostName + ":" + client.getPort
-        val handler = new Runnable {
-          def run {
-            val echo = Echo(client)
-            val result = Runner.run(echo) { echo =>
-              echo.in match {
-                case Request(text)  => echo ! Reply(text, text.filter(_.isLetter).size)
-                case Tag(msg)       => echo ! ErrorMessage(msg, "Unexpected message '" + msg + "', expected Request (0).")
-              }
-            }
-            result.left.toOption.map(_.getMessage).foreach(println)
-            println("Closed connection to " + whois)
-          }
-        }
-        if (!cancel) {
-          println("Request from " + whois)
-          new Thread(handler).start
-        }
+  def handleClient(socket: Socket) {
+    val echo = Echo(socket)
+    val result = Runner.run(echo) { echo =>
+      echo.in match {
+        case Request(text)  => echo ! Reply(text, text.filter(_.isLetter).size)
+        case Tag(msg)       => echo ! ErrorMessage(msg, "Unexpected message '" + msg + "', expected Request (0).")
       }
-    } finally {
-      socket.close
-      println("EchoServer stopped")
     }
-  }
-
-  def start() {
-    cancel = false
-    val server = new Thread(this)
-    server.setDaemon(true)
-    server.start
-  }
-
-  def stop() {
-    cancel = true
-    try {
-      new Socket("localhost", port).close
-    } catch {
-      case e: Exception => println(e.getMessage)
-    }
+    result.left.toOption.map(_.getMessage).foreach(println)
   }
 }
---
+++ src/main/scala/twp4scala/server.scala
@@ -0,0 +1,75 @@
+package twp4scala
+
+import java.io.{Console => _, _}
+import java.net._
+import twp4scala._
+import twp4scala.protocol.echo._
+
+trait TcpServer extends Runnable {
+
+  @volatile private var cancel = false
+
+  val port: Int
+
+  def handleClient(socket: Socket): Unit
+
+  def run() {
+    val socket = new ServerSocket(port)
+    serverStarted(socket)
+    try {
+      while (!cancel) {
+        val client = socket.accept
+        val handler = new Runnable {
+          def run {
+            handleClient(client)
+            connectionClosed(client)
+          }
+        }
+        if (!cancel) {
+          connectionOpened(client)
+          new Thread(handler).start
+        }
+      }
+    } catch {
+      case e: Exception => {
+        Console.err.println("Server crashed")
+        e.printStackTrace()
+      }
+    } finally {
+      serverStopped(socket)
+      try { socket.close } catch { case _ => }
+    }
+  }
+
+  def start() {
+    cancel = false
+    val server = new Thread(this)
+    server.setDaemon(true)
+    server.start
+  }
+
+  def stop() {
+    cancel = true
+    try {
+      new Socket("localhost", port).close
+    } catch {
+      case e: Exception => println(e.getMessage)
+    }
+  }
+
+  def serverStarted(server: ServerSocket) {
+    println("Server listening on port " + port + " ...")
+  }
+
+  def serverStopped(server: ServerSocket) {
+    println("EchoServer stopped")
+  }
+
+  def connectionOpened(socket: Socket) {
+    println("Request from " + socket.getInetAddress.getHostName + ":" + socket.getPort)
+  }
+
+  def connectionClosed(socket: Socket) {
+    println("Closed connection to " + socket.getInetAddress.getHostName + ":" + socket.getPort)
+  }
+}
