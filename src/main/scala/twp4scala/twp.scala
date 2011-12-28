6ae950317e6a024acd06bc09be4b4505d919e921
Markus Kahl <machisuji@googlemail.com>
2011-12-28 15:52:33 +0100

    possibly persistent (fault tolerant) connection

--- src/main/scala/twp4scala/twp.scala
+++ src/main/scala/twp4scala/twp.scala
@@ -2,6 +2,7 @@ package twp4scala
 
 import scala.Either
 import java.lang.IllegalStateException
+import java.io.IOException
 
 object Twp {
   def apply[T <: AbstractProtocol, S](proto: T)(block: T => S): Either[Exception, S] = {
@@ -26,6 +27,67 @@ object Twp {
   private[twp4scala] def log(msg: String) = {
     if (Twp.debug) println("[DEBUG] " + msg)
   }
+}
+
+/**
+ * Can be used to perform Twp IO while automatically
+ * reconnecting in case the connection is broken.
+ */
+abstract class PersistentConnection[T <: AbstractProtocol] {
+  private var connection: Option[T] = None
+  private var retryCount: Int = 0
+
+  def open: T
+  def close = {
+    connection.foreach(_.shutdown)
+    connection = None
+  }
+
+  def reconnect() {
+    connection = Some(open)
+    connection.foreach(_.initiate)
+    Twp.log("Reconnecting " + this)
+  }
+
+  /**
+   * Runs the given block using this connection.
+   * Returns either an Exception, if one was thrown, or the actual result.
+   */
+  def apply[S](block: T => S): Either[Exception, S] = {
+    if (!connection.isDefined) {
+      reconnect()
+    }
+    try {
+      Right(block(connection.get))
+    } catch {
+      case e: IOException if retryCount == 0 => { // prolly connection broken, try once again
+        connection = None
+        retryCount += 1
+        apply(block)
+      }
+      case e: Exception => {
+        retryCount = 0
+        e.printStackTrace()
+        Left(e)
+      }
+    }
+  }
+
+  def get[S](twpResult: Either[Exception, S]): S = {
+    val error = twpResult.left.toOption
+    error map { err =>
+      if (Twp.debug) err.printStackTrace();
+      else println(err.getMessage);
+      null.asInstanceOf[S]} getOrElse twpResult.right.get
+  }
+
+  /**
+   * While #apply will return either an Exception or the actual result,
+   * get will return the result or null if an Exception was thrown.
+   *
+   * The Exception's error message will be printed to stdout.
+   */
+  def get[S](block: T => S): S = get(apply(block))
 }
 
 import Twp.{logr, logw, log}
