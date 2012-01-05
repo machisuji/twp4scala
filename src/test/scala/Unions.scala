542a99eca044775f5866905569726c5e2aa89745
Markus Kahl <machisuji@googlemail.com>
2012-01-05 11:30:13 +0100

    started unions

---
+++ src/test/scala/Unions.scala
@@ -0,0 +1,28 @@
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.Spec
+import twp4scala.{TwpReader, TwpConversions}
+
+class Unions extends Spec with ShouldMatchers {
+
+  type Path = Seq[String]
+  object Path extends TwpReader with TwpConversions {
+    def in(implicit in: twp4scala.Input) = sequence[Path]
+  }
+
+  trait File[T] // Union: Path | String
+  object File {
+    implicit object PathWitness extends File[Path]
+    implicit object StringWitness extends File[String]
+  }
+
+  describe("Parsing") {
+    def read[T: File](value: T) = value match {
+      case _: Seq[_] => "Path" // Path instead of Seq[_] would cause an erasure warning
+      case _: String => "String"
+    }
+    read(Seq("/")) should equal ("Path")
+    read("/etc/hosts") should equal ("String")
+    // read(Seq(42)) and
+    // read(false) and everything else without the expected union types won't compile, yay!
+  }
+}
