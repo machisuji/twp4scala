574c99f5700014c21cce555111e4295052da05d0
Markus Kahl <machisuji@googlemail.com>
2012-01-11 21:12:15 +0100

    protected -> public

--- src/main/scala/twp4scala/TwpWriter.scala
+++ src/main/scala/twp4scala/TwpWriter.scala
@@ -1,35 +1,34 @@
 package twp4scala
 
-import tools.ast.ApplicationType
 import Twp.{log, logw}
 
 trait TwpWriter extends ByteOperations {
 
-  protected def tag(tagType: Int) = {
+  def tag(tagType: Int) = {
     log("tag(%d)" format tagType)
     Array(tagType.toByte)
   }
 
-  protected def endOfContent = {
+  def endOfContent = {
     log("endOfContent")
     Array(0.toByte)
   }
-  protected def noValue = {
+  def noValue = {
     log("noValue")
     Array(1.toByte)
   }
 
-  protected def sequence: Array[Byte] = {
+  def sequence: Array[Byte] = {
     log("sequence")
     Array(3.toByte)
   }
 
-  protected def sequence[T <: TwpWritable](seq: Seq[T]): Array[Byte] = {
+  def sequence[T <: TwpWritable](seq: Seq[T]): Array[Byte] = {
     logw(seq.toString)
     this.sequence ++ (seq.flatMap(_.write).flatten.toArray ++ endOfContent)
   }
 
-  protected def message(id: Int): Array[Byte] = {
+  def message(id: Int): Array[Byte] = {
     if (id >= 0 && id <= 7) {
       logw("message(%d) (%d)" format (id,  id + 4))
       (4 + id).getBytes(1)
@@ -39,18 +38,18 @@ trait TwpWriter extends ByteOperations {
     }
   }
 
-  protected def shortInt(i: Int) = {
+  def shortInt(i: Int) = {
     logw("shortInt(%d)" format i)
     tag(13) ++ i.getBytes(1)
   }
-  protected def longInt(i: Int) = {
+  def longInt(i: Int) = {
     logw("longInt(%d)" format i)
     tag(14) ++ i.getBytes(4)
   }
 
-  protected def someInt(i: Int) = if (i >= 256) longInt(i) else shortInt(i)
+  def someInt(i: Int) = if (i >= 256) longInt(i) else shortInt(i)
 
-  protected def string(str: String): Array[Byte] = {
+  def string(str: String): Array[Byte] = {
     logw("String(%s)" format str)
     val data = str.getBytes("UTF-8")
     val (msgTag, prefix) =
@@ -59,7 +58,7 @@ trait TwpWriter extends ByteOperations {
     msgTag ++ prefix ++ data
   }
 
-  protected def binary(data: Array[Byte]): Array[Byte] = {
+  def binary(data: Array[Byte]): Array[Byte] = {
     logw("binary(%s)" format data.toString)
     val (msgTag, prefix) =
       if (data.size <= 0xFF) tag(15) -> data.size.getBytes(1)
@@ -67,8 +66,10 @@ trait TwpWriter extends ByteOperations {
     msgTag ++ prefix ++ data
   }
 
-  protected def nop = {
+  def nop = {
     log("nop")
     new Array[Byte](0)
   }
 }
+
+object TwpWriter extends TwpWriter
