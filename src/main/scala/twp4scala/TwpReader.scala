09eab49a0d089d9fb2b4957e8e81c5a223c75ee7
Markus Kahl <machisuji@googlemail.com>
2012-01-11 21:01:14 +0100

    try reader functions

--- src/main/scala/twp4scala/TwpReader.scala
+++ src/main/scala/twp4scala/TwpReader.scala
@@ -36,55 +36,78 @@ trait TwpReader extends ByteOperations {
     } else throw new IllegalStateException("Expected to read message/extension, got: " + res)
   }
 
-  def string(implicit in: Input) = in.read match {
+  def tryString(implicit in: Input) = in.read match {
     case short if short >= 17 && short <= 126 => {
       val ret = new String(in.take(short - 17), "UTF-8")
-      logr("short string \"%s\"" format ret); ret
+      logr("short string \"%s\"" format ret); Right(ret)
     }
     case long if long == 127 => {
       val size = in.take(4).toInt
       val ret = new String(in.take(size), "UTF-8")
-      logr("long string \"%s\"" format ret); ret
+      logr("long string \"%s\"" format ret); Right(ret)
     }
-    case tag => throw new RuntimeException("Expected string, got: " + tag)
+    case tag => Left(tag)
+  }
+  def string(implicit in: Input) = {
+    val str = tryString
+    str.left.foreach(tag => throw new RuntimeException("Expected string, got: " + tag))
+    str.right.get
   }
 
-  def shortInt(implicit in: Input) = in.read match {
+  def tryShortInt(implicit in: Input) = in.read match {
     case 13 => {
       val ret = in.read
-      logr("short int " + ret); ret
+      logr("short int " + ret); Right(ret)
     }
-    case tag => throw new RuntimeException("Expected short int, got: " + tag)
+    case tag => Left(tag)
+  }
+  def shortInt(implicit in: Input) = {
+    val short = tryShortInt
+    short.left.foreach(tag => throw new RuntimeException("Expected short int, got: " + tag))
+    short.right.get
   }
 
-  def longInt(implicit in: Input) = in.read match {
+  def tryLongInt(implicit in: Input) = in.read match {
     case 14 => {
       val ret = in.take(4).toInt
-      logr("long int " + ret); ret
+      logr("long int " + ret); Right(ret)
     }
-    case tag => throw new RuntimeException("Expected long int, got: " + tag)
+    case tag => Left(tag)
+  }
+  def longInt(implicit in: Input) = {
+    val long = tryLongInt
+    long.left.foreach(tag => throw new RuntimeException("Expected long int, got: " + tag))
+    long.right.get
   }
 
-  def someInt(implicit in: Input) = in.read match {
-    case 13 => { in.unread(13); shortInt }
-    case 14 => { in.unread(14); longInt }
-    case tag => throw new RuntimeException("Expected int, got: " + tag)
+  def trySomeInt(implicit in: Input) = in.read match {
+    case 13 => { in.unread(13); Right(shortInt) }
+    case 14 => { in.unread(14); Right(longInt) }
+    case tag => Left(tag)
+  }
+  def someInt(implicit in: Input) = {
+    val int = trySomeInt
+    int.left.foreach(tag => throw new RuntimeException("Expected int, got: " + tag))
+    int.right.get
   }
 
-  def binary(implicit in: Input) = {
+  def tryBinary(implicit in: Input) = {
     val tg = tag
     val size =
       if (tg == 15) in.take(1).toInt
       else if (tg == 16) in.take(4).toInt
-      else throw new RuntimeException("Expected binary, got: " + tag)
-
-    val ret = in.take(size)
-    logr("binary " + ret); ret
+      else -1
+    if (size != -1) {
+      val ret = in.take(size)
+      logr("binary " + ret); Right(ret)
+    } else {
+      Left(tg)
+    }
   }
-
-  def sequence[S](implicit in: Input, reader: TwpReadable[S]): S = {
-    val ret = reader.read(in)
-    logr("sequence " + ret); ret
+  def binary(implicit in: Input) = {
+    val bin = tryBinary
+    bin.left.foreach(tag => throw new RuntimeException("Expected binary, got: " + tag))
+    bin.right.get
   }
 
   def peek(in: Input): Int = {
