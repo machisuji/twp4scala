47c8bac8065be171c68f715690acc23abcb86f62
Markus Kahl <machisuji@googlemail.com>
2012-01-14 02:14:29 +0100

    Some more useful debug methods.

--- src/main/scala/twp4scala/tools/Debugger.scala
+++ src/main/scala/twp4scala/tools/Debugger.scala
@@ -10,4 +10,28 @@ object Debugger {
     }.foreach(byte => print(" " + byte))
     println()
   }
+
+  def getCurrentStackTrace(number: Int, stackTraceOffset: Int = 0): Seq[StackTraceElement] = {
+    require(stackTraceOffset >= 0)
+    var e: Exception = null
+    try { throw new Exception("log") } catch { case ex: Exception => e = ex }
+    val st = e.getStackTrace
+    val start = 1 + stackTraceOffset
+    for {
+      i <- start to (start + number - 1)
+      line = st.apply(i + stackTraceOffset)
+    } yield line
+  }
+
+  def getCurrentStackTraceElement(stackTraceOffset: Int = 0) = getCurrentStackTrace(1, stackTraceOffset + 1).head
+
+  def getLines(number: Int, stackTraceOffset: Int = 0) = getCurrentStackTrace(number, stackTraceOffset + 1).map(ste =>
+    ste.getFileName + ":" + ste.getLineNumber)
+
+  def getLine(stackTraceOffset: Int = 0) = getLines(1, stackTraceOffset + 1).head
+
+  def testGetLine {
+    println("Tests getLine")
+    println("Line: " + getLine())
+  }
 }
