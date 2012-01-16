7b22aa692ee0f9fc610ab16f00135b28b574a08d
Markus Kahl <machisuji@googlemail.com>
2012-01-16 12:35:25 +0100

    all benchmarks implemented

--- src/main/scala/twp4scala/example/Benchmark.scala
+++ src/main/scala/twp4scala/example/Benchmark.scala
@@ -22,7 +22,7 @@ object Benchmark {
           echo.in match {
             case Reply(text, letters) => if (letters != 0) {
               println("empty echo request yielded invalid response: Reply(%s, %d)".format(text, letters))
-            } else print(".")
+            }
             case _ => println("empty echo request %d failed".format(i))
           }
         }.left.foreach { e =>
@@ -33,6 +33,48 @@ object Benchmark {
       println()
       val result = System.currentTimeMillis() - ms
       println("Time for 100k local, empty echo requests: " + result + " ms")
+      echoServer.stop()
+    }
+  }
+
+  def remote1kEchoRequests() {
+    val ms = System.currentTimeMillis
+    Twp(Echo("www.dcl.hpi.uni-potsdam.de", 80)) { echo =>
+      for (i <- 1 to 1000) {
+        echo ! Request("")
+        echo.in match {
+          case Reply(text, letters) => if (letters != 0) println("invalid echo response")
+          case _ => throw new RuntimeException("Invalid response")
+        }
+      }
+    }
+    val result = System.currentTimeMillis - ms
+    println("Time for 1000 remote echo requests: " + result + " ms")
+  }
+
+  def local10kEchoRequests() {
+    val echoServer = new EchoServer(9000)
+    val string = Iterator.fill(10000)(" ").mkString
+    echoServer.start()
+    val ms = System.currentTimeMillis
+    try {
+      for (i <- 1 to 10000) {
+        Twp(Echo("localhost", echoServer.port)) { echo =>
+          echo ! Request(string)
+          echo.in match {
+            case Reply(text, letters) => if (letters != 0) {
+              println("empty echo request yielded invalid response: Reply(%s, %d)".format(text, letters))
+            }
+            case _ => println("empty echo request %d failed".format(i))
+          }
+        }.left.foreach { e =>
+          println("100k local echo requests test failed: " + e.getClass + ": " + e.getMessage)
+        }
+      }
+    } finally {
+      println()
+      val result = System.currentTimeMillis() - ms
+      println("Time for 10k local echo requests: " + result + " ms")
       echoServer.stop()
     }
   }
