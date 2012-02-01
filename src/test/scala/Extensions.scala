13539ff1a58733af7b975d6d9b41e17d4d1d93a5
Markus Kahl <machisuji@googlemail.com>
2012-02-01 16:42:12 +0100

    removed clutter from ext test

--- src/test/scala/Extensions.scala
+++ src/test/scala/Extensions.scala
@@ -1,4 +1,3 @@
-import java.io.ByteArrayInputStream
 import org.scalatest.matchers.ShouldMatchers
 import org.scalatest.Spec
 import twp4scala._
@@ -9,7 +8,6 @@ class Extensions extends Spec with ShouldMatchers {
     it("should be read along with normal messages") {
       val sig = new Signature(Array[Byte](1, 2, 3))
       val input = DebugProtocol(ErrorMessage(42, "foobar").out.init ++ sig.out)
-      println("data: " + input.input.mkString(" "))
       input.in match {
         case ErrorMessage(tag, msg) => msg should equal ("foobar")
         case _ => fail("Could not read ErrorMessage")
