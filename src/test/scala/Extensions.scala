5d3cbec38bfabff094c5492833dda93aa8e0a414
Markus Kahl <machisuji@googlemail.com>
2012-02-02 14:41:00 +0100

    test for implicit extension send

--- src/test/scala/Extensions.scala
+++ src/test/scala/Extensions.scala
@@ -1,7 +1,7 @@
 import org.scalatest.matchers.ShouldMatchers
 import org.scalatest.Spec
+import twp4scala.tools.{Debugger, MemoryProtocol}
 import twp4scala._
-import twp4scala.tools.MemoryProtocol
 
 class Extensions extends Spec with ShouldMatchers {
   describe("Extension Messages") {
@@ -27,6 +27,36 @@ class Extensions extends Spec with ShouldMatchers {
       }
       input.in.lastExtension.flatMap(Signature.from).map(ext =>
         ext.data.toSeq should equal (sig.data.toSeq)) getOrElse fail("no extension sent")
+    }
+
+    it("should be semi-implicitly sendable, too") {
+      val ext = Any("Hallo", 704)
+      val data = MemoryProtocol()
+
+      data !! ErrorMessage(99, "breee")
+      data.in match {
+        case ErrorMessage(tag, _) => tag should equal (99)
+        case _ => fail("no ErrorMessage sent")
+      }
+      data.in.lastExtension should not be ('defined)
+
+      data.send(ErrorMessage(100, "bree"), Some(ext))
+      data.in match {
+        case ErrorMessage(_, msg) => msg should equal ("bree")
+        case _ => fail("no ErrorMessage sent")
+      }
+      data.in.lastExtension should be ('defined)
+
+      data !! ErrorMessage(1, "this is getting old") // should send along the lastExtension and consume it
+      data.in.lastExtension should not be ('defined)
+      data.in match {
+        case ErrorMessage(tag, msg) => {
+          tag should equal (1)
+          msg should include ("old")
+        }
+        case _ => fail("no ErrorMessage sent")
+      }
+      data.in.lastExtension.map(_ should equal (ext)) getOrElse fail("no extension sent")
     }
   }
 }
