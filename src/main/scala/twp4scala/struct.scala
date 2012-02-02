8f2ab71ae3dcdfa696a0271a43bd9d2c66568d61
Markus Kahl <machisuji@googlemail.com>
2012-02-02 14:38:19 +0100

    made LooseStruct a case class (for equals and stuff)

--- src/main/scala/twp4scala/struct.scala
+++ src/main/scala/twp4scala/struct.scala
@@ -15,7 +15,7 @@ trait StructCompanion[S <: Struct, T] extends MessageCompanion[S, T] {
   override def isDefinedAt(implicit in: Input) = Preview.check(tag ==)
 }
 
-class LooseStruct(val values: Seq[Any], extensionId: Option[Int] = None) extends TwpWriter
+case class LooseStruct(val values: Seq[Any], extensionId: Option[Int] = None) extends TwpWriter
     with TwpConversions with TwpWritable with CanBeExtension {
   val End = Stream(Array(0.toByte))
 
